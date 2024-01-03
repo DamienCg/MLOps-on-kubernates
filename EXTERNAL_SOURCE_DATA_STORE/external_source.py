06a530cda44a9798a9a989c6c067d3ad81cd871d
Dcg <damienciagola@gmail.com>
2024-01-03 16:29:37 +0100

    add file

--- EXTERNAL_SOURCE_DATA_STORE/external_source.py
+++ EXTERNAL_SOURCE_DATA_STORE/external_source.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'DATI DA FONTE ESTERNA (UNI)!'
+def get_data():
+    try:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir, 'data_file.csv')
+        if file_path.endswith('.csv'):
+            data = pd.read_csv(file_path)
+        elif file_path.endswith('.xlsx'):
+            data = pd.read_excel(file_path)
+        else:
+            raise ValueError("Il formato del file non è supportato.")
+
+        json_data = data.to_json(orient='records')
+        return jsonify({"data": json_data})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
